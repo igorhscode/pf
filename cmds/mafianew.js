e3996f41cb8b683a01d24f097f443d2a4efb3dbf
igorhscode <59455439+igorhscode@users.noreply.github.com>
2020-02-18 19:06:34 +0300

    Update mafianew.js

--- cmds/mafianew.js
+++ cmds/mafianew.js
@@ -3,9 +3,8 @@ const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
     let role = message.guild.roles.find(role => role.name === "◦  🎉 › Event")
     let role1 = message.guild.roles.find(role => role.name === "· • ♡ EventMod ♡ • ·")
-    let em1 = message.guild.emojis.find(emoji => emoji.name === "fig") 
-    let em2 = message.guild.emojis.find(emoji => emoji.name === "heart")
-    let em3 = message.guild.emojis.find(emoji => emoji.name === "animated_cross")
+    let em1 = message.guild.emojis.find(emoji => emoji.name === "heart1")
+    let em2 = message.guild.emojis.find(emoji => emoji.name === "heart2")
     if(!message.member.hasPermission("SEND_MESSAGES")) return message.channel.send(`❌ У вас нет прав для использования этой команды!!`);
     if(!args[0]) return bot.send(`❌ Вы не указали Содержимое!`);
     let botmessage = args.join(" ");
@@ -13,7 +12,7 @@ module.exports.run = async (bot,message,args) => {
     .setAuthor("Pink Flame", "https://cdn.discordapp.com/icons/586825171168526336/b9dc978e80594f775692fd8273341336.jpg")
     .setColor([255,255,0])
     .setDescription(`
-   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀**М А Ф И Я**
+   ⠀⠀⠀⠀⠀⠀⠀⠀⠀${em1}⠀**М А Ф И Я** ${em2}
 
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀${role}
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Ведущий: <@${message.author.id}>
@@ -21,10 +20,10 @@ module.exports.run = async (bot,message,args) => {
 
 В самом начале ${role1} распределяет роли в лс участникам. Мирный житель, Полицейский, Доктор, Мафия и Дон. Или же вас могут добавить в закрытые чаты ролей.Если Вам не написали в личные сообщения роль, то Вы являетесь мирным жителем. 
 
-⠀⠀⠀⠀⠀⠀⠀. ︶︶︶. ̥ᔗ  [Подключиться](https://discord.gg/KZ3e8AR)  ᔚ ̥ . ︶︶︶.
+⠀⠀⠀⠀⠀⠀${em1} . ︶︶︶. ̥ᔗ  [Подключиться](https://discord.gg/KZ3e8AR)  ᔚ ̥ . ︶︶︶. ${em2}
 
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀˗ˏˋ☆ Победа ⠀⠀⠀⠀⠀⠀⠀˗ˏˋ☆ Участие⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀— 350 монет 🍬 ⠀⠀⠀⠀⠀⠀⠀ ⠀⠀— 100 монет 🍬⠀⠀⠀⠀⠀⠀⠀⠀⠀`)
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀— 350 монет 🍬 ⠀⠀⠀⠀⠀— 100 монет 🍬⠀⠀⠀⠀⠀⠀⠀⠀⠀`)
     .setImage('https://cdn.discordapp.com/attachments/678339460383113250/678515265427996672/4444.png')
     message.delete().catch();
     bot.send(embed);
