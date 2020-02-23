56e3c3b14ccf29ce0cc5d29f472eef5b643bd623
igorhscode <59455439+igorhscode@users.noreply.github.com>
2020-02-23 11:04:57 +0300

    Update buy.js

--- cmds/buy.js
+++ cmds/buy.js
@@ -2,68 +2,46 @@ const Discord =  require('discord.js')
 const db =  require('quick.db')
 module.exports.run = async (bot,message,args) => {
     
-    
+    let em3 = message.guild.emojis.find(emoji => emoji.name === "yes")
+    let em4 = message.guild.emojis.find(emoji => emoji.name === "not")
     let author = db.fetch(`candy_${message.author.id}`)
-   if(!args[0]) {
-    return message.channel.send("❌ Вы не правильно написали команду. Попробуйте так: `pf!buy [номер роли]`")
-   }
+
     if (args[0] == '1') {
-        if (author < 13000) return message.channel.send('❌ У вас не хватает 13000 🍬  для покупки этой роли') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
-        
-        message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "Пушистая"))
-        db.subtract(`candy_${message.author.id}`, 13000)
-        message.channel.send(message.author.tag + '✔️ Вы успешно купили данную роль за  `13000 🍬 `')
-    } else if(args[0] == '2') {
-        if (author < 10000) return message.channel.send('❌ У вас не хватает 10000 🍬  для покупки этой роли') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
+        if (author < 13000) return message.channel.send(`${em4} У вас не хватает 🍬 для покупки этой роли`) // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
         
         message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "Хомячок"))
 
-        db.subtract(`candy_${message.author.id}`, 10000)
-        message.channel.send(message.author.tag + '✔️ Вы успешно купили данную роль за  `10000 🍬 `')
-    } else if(args[0] == '3') {
-        if (author < 7800 ) return message.channel.send('❌ У вас не хватает 7800 🍬  для покупки этой роли') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
-        
-        message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "эхххъ сейчас бы тяночку."))
-
-        db.subtract(`candy_${message.author.id}`, 7800 )
-        message.channel.send(message.author.tag + '✔️ Вы успешно купили данную роль за  `7800  🍬 `')
-    } else if(args[0] == '4') {
-        if (author < 5000) return message.channel.send('❌ У вас не хватает 5000 🍬  для покупки этой роли') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
-        
-        message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "эхххъ сейчас бы куна."))
-
-        db.subtract(`candy_${message.author.id}`, 5000)
-        message.channel.send(message.author.tag + '✔️ Вы успешно купили данную роль за  `5000 🍬 `')
-    } else if(args[0] == '5') {
-        if (author < 8000) return message.channel.send('❌ У вас не хватает 8000 🍬  для покупки этой роли') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
+        db.subtract(`candy_${message.author.id}`, 13000)
+        message.channel.send(message.author.tag + `${em3} Вы успешно купили данную роль за  13000 🍬`)
+    } else if(args[0] == '2') {
+        if (author < 10000) return message.channel.send(`${em4} У вас не хватает 🍬 для покупки этой роли`) // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
         
         message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "Твоя лубоффф"))
 
-        db.subtract(`candy_${message.author.id}`, 8000)
-        message.channel.send(message.author.tag + '✔️ Вы успешно купили данную роль за  `8000 🍬 `')
-    } else if(args[0] == '6') {
-        if (author < 6700) return message.channel.send('❌ У вас не хватает 6700 🍬  для покупки этой роли') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
+        db.subtract(`candy_${message.author.id}`, 10000)
+        message.channel.send(message.author.tag + `${em3} Вы успешно купили данную роль за  10000 🍬`)
+    } else if(args[0] == '3') {
+        if (author < 14000 ) return message.channel.send(`${em4} У вас не хватает 🍬 для покупки этой роли`) // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
         
-        message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "Сын маминой подруги"))
+        message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "эхххъ сейчас бы тяночку."))
 
-        db.subtract(`candy_${message.author.id}`, 6700)
-        message.channel.send(message.author.tag + ' ✔️ Вы успешно купили данную роль за  `6700 🍬 `')
-} else if(args[0] == '7') {
-    if (author < 9000) return message.channel.send('❌ У вас не хватает 9000 🍬  для покупки этой роли') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
-    
-    message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "sleepy baby 🍒"))
+        db.subtract(`candy_${message.author.id}`, 14000 )
+        message.channel.send(message.author.tag + `${em3} Вы успешно купили данную роль за 14000  🍬`)
+    } else if(args[0] == '4') {
+        if (author < 12000) return message.channel.send(`${em4} У вас не хватает 🍬 для покупки этой роли`) // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
+        
+        message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "эхххъ сейчас бы куна."))
 
-    db.subtract(`candy_${message.author.id}`, 9000)
-    message.channel.send(message.author.tag + '✔️ Вы успешно купили данную роль за  `9000 🍬 `')
-} else if(args[0] == '8') {
-    if (author < 7800) return message.channel.send('❌ У вас не хватает 7800 🍬  для покупки этой роли') // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
-    
-    message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "save me in your heart 💗"))
+        db.subtract(`candy_${message.author.id}`, 12000)
+        message.channel.send(message.author.tag + `${em3} Вы успешно купили данную роль за  12000 🍬`)
+    } else if(args[0] == '5') {
+        if (author < 11700) return message.channel.send(`${em4} У вас не хватает 🍬 для покупки этой роли`) // if the authors balance is less than 700$ return this, since the role costs 700$ in the store
+        
+        message.guild.members.get(message.author.id).addRole(message.guild.roles.find(role => role.name === "save me in your heart 💗"))
 
-    db.subtract(`candy_${message.author.id}`, 7800)
-    message.channel.send(message.author.tag + ' ✔️ Вы успешно купили данную роль за  `7800 🍬 `')
-
-    }
+        db.subtract(`candy_${message.author.id}`, 11700)
+        message.channel.send(message.author.tag + `${em3} Вы успешно купили данную роль за  11700 🍬`)
+        }
 }
 module.exports.help = {
     name: "buy"
