878844cbd34d824345383a3056010d90ab5545b3
igorhscode <59455439+igorhscode@users.noreply.github.com>
2020-02-23 11:16:01 +0300

    Update warn.js

--- cmds/warn.js
+++ cmds/warn.js
@@ -2,17 +2,19 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 let profile = require("../profile.json");
 module.exports.run = async (bot,message,args) =>{
+    let em3 = message.guild.emojis.find(emoji => emoji.name === "yes")
+    let em4 = message.guild.emojis.find(emoji => emoji.name === "not")
     try{
         function send (msg){
             message.channel.send(msg);
         }
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("❌ У вас нет прав");
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(`${em4} У вас нет прав`);
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-    if(!args[0]) return send("❌ Вы не указали пользователя");
-    if(!rUser) return send("❌ Пользователь не найден");
+    if(!args[0]) return send(`${em4} Вы не указали пользователя`);
+    if(!rUser) return send(`${em4} Пользователь не найден`);
     if(rUser.hasPermission("MANAGE_MESSAGES"))
-    return message.reply("❌ нее... Вы не можете дать ему предупреждение!");
+    return message.reply(`${em4} нее... Вы не можете дать ему предупреждение!`);
 
     profile[rUser.id].warns++;
     fs.writeFile('../profile.json',JSON.stringify(profile),(err)=>{
